80ddbd33d2c4f0abb3057151d58f1addcd6ac39a
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2019-11-13 10:50:22 +0100

    add initial files

---
+++ src/server/index.js
@@ -0,0 +1,51 @@
+const express = require('express'),
+    expressLogging = require('express-logging'),
+    logger = require('logops');
+
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+//const homedir = require('os').homedir();
+
+const app = express();
+app.use(expressLogging(logger));
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+const srcPath = __dirname + '/';
+
+app.get('/api/test/*', function (req, res) {
+  res.send(__dirname);
+  res.send('Got a GET request')
+})
+
+/*
+app.get('/api/list/*', function (req, res) {
+  const fileList = fs.readdirSync(srcPath);
+  fileList = fileList.filter(function(x) {
+    return x.match(/\.js$/);
+  });
+  fileList.sort();
+  res.send(JSON.stringify(fileList));
+});
+*/
+
+app.get('/api/read/*', function (req, res) {
+  const fname = path.basename(req.path);
+  fname = path.join(srcPath, fname);
+  res.send(fs.readFileSync(fname));
+});
+
+app.post('/api/write/*', function (req, res) {
+  const bname = path.basename(req.path);
+  const fname = path.join(srcPath, bname);
+  const doc = req.body.doc;
+  fs.writeFileSync(fname, doc);
+  //throw new Error("broken");
+  res.send('Saved file: ' + bname);
+});
+
+app.listen(8080);
